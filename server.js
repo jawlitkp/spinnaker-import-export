87813abf14367ed94a43991249a184ad1e4b785c
moondev <chad.d.moon@gmail.com>
2015-12-09 17:07:26 -0700

    readme updates

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var sys = require('sys')
-var exec = require('child_process').exec;
+var multer  = require('multer');
+var sys = require('sys');
 var fs = require('fs');
+var exec = require('child_process').exec;
+var upload = multer({ dest: 'imports/' });
 
 var app = express();
 app.set('views', './views')
@@ -12,26 +14,22 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/export', function(req, res) {
-  exec("cqlsh -f export.cql", function (error, stdout, stderr) {
-    exec("cd exports && tar -zcvf ../public/spinnaker_export.tgz", function (error, stdout, stderr) {
-      res.redirect('/spinnaker_export.tgz');
+app.post('/export', function(req, res) {
+  exec("cqlsh -f export.cql", extract);
+});
+
+app.post('/import', upload.single('csv'), function(req, res) {
+  exec("cd imports && tar -xvf " + req.file.filename, function (error, stdout, stderr) {
+    exec("cqlsh -f import.cql", function (error, stdout, stderr) {
+      res.send('Import Complete');
     });
   });
 });
 
-app.post('/import', function(req, res) {
-  console.log(req.files);
-  fs.readFile(req.files.csv.path, function (err, data) {
-  var newPath = __dirname + "spinnaker_import.tgz";
-  fs.writeFile(newPath, data, function (err) {
-    exec("tar -xf spinnaker_import.tgz -C import", function (error, stdout, stderr) {
-      exec("cqlsh -f import.cql", function (error, stdout, stderr) {
-       res.send('done');
-    });
-    });
+var extract = function (error, stdout, stderr){
+  exec("cd exports && tar -zcvf ../public/spinnaker_export.tgz", function (error, stdout, stderr) {
+    res.redirect('/spinnaker_export.tgz');
   });
-});
-});
+};
 
-app.listen(9000);
+app.listen(10000);
